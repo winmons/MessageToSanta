813a84aa78e904b1423a6837e63dd0f23447a5b4
Vyacheslav Nechiporenko <winmons@gmail.com>
2013-06-18 14:32:47 +0300

    test commit

---
+++ MessageToSanta/MTSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTSAppDelegate.h
+//  MessageToSanta
+//
+//  Created by Admin on 6/18/13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MessageToSanta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MessageToSanta
+//
+//  Created by Admin on 6/18/13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTSAppDelegate class]));
+    }
+}
