27cb65771c7dc08fbcfb065684a275fb23953017
Vyacheslav Nechiporenko <winmons@gmail.com>
2013-06-25 14:40:43 +0300

    project structure changes

---
+++ MessageToSanta/MTSMenuViewController.m
@@ -0,0 +1,38 @@
+//
+//  MTSMenuViewController.m
+//  MessageToSanta
+//
+//  Created by Admin on 6/25/13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import "MTSMenuViewController.h"
+
+@interface MTSMenuViewController ()
+
+@end
+
+@implementation MTSMenuViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
